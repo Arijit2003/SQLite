27645c2f6e12495b07272d5f09dabe1af645570a
Arijit2003 <arijitmodak2003@gmail.com>
2023-04-30 23:49:17 +0530

    Contact App+UI

--- app/src/main/java/com/example/onlinedatabase/MainActivity.java
+++ app/src/main/java/com/example/onlinedatabase/MainActivity.java
@@ -1,51 +1,123 @@
 package com.example.onlinedatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 
+import com.example.onlinedatabase.data.MyAdapter;
 import com.example.onlinedatabase.data.MyDBHandler;
 import com.example.onlinedatabase.model.CONTACT;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static RecyclerView recyclerView;
+    public static  ArrayList<CONTACT> contactArrayList;
+    private FloatingActionButton addNewContactFAB;
+    private MyDBHandler dbHandler;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recyclerView);
+        addNewContactFAB=findViewById(R.id.addNewContactFAB);
 
-        MyDBHandler dbHandler = new MyDBHandler(MainActivity.this);
-        CONTACT arijit=new CONTACT("ArijitModak","6295401465");
-        dbHandler.addContact(arijit);
+        dbHandler = new MyDBHandler(MainActivity.this);
+//        CONTACT arijit=new CONTACT("ArijitModak","6295401465");
+//        dbHandler.addContact(arijit);
+//
+//        CONTACT braj=new CONTACT("BrajKishorSharma","7895647026");
+//        dbHandler.addContact(braj);
+//
+//        CONTACT devashish=new CONTACT("DevashishDhaulakhandi","6985478722");
+//        dbHandler.addContact(devashish);
+//
+//        devashish.setId(15);
+//        devashish.setName("Deva Dhalu");
+//        devashish.setContact("2456186206");
+//        int affectedRows=dbHandler.updateContact(devashish);
+//        Log.d("Affected Rows",""+affectedRows);
+//
+//
+//
+//        List<CONTACT> allContacts=dbHandler.getAllContacts();
+//        Log.d("SIZE_OF_CONTACTS",""+allContacts.size());
+//        for(CONTACT contact:allContacts){
+//            Log.d("ALL_TYPE_CONTACT",contact.getId()+" "+contact.getName()+" "+contact.getContact());
+//        }
+//
+//        int dr=dbHandler.deleteContact(16);
+//        Log.d("Deleted Rows",""+dr);
+        addNewContactFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddContactDialog();
+            }
+        });
 
-        CONTACT braj=new CONTACT("BrajKishorSharma","7895647026");
-        dbHandler.addContact(braj);
-
-        CONTACT devashish=new CONTACT("DevashishDhaulakhandi","6985478722");
-        dbHandler.addContact(devashish);
-
-        devashish.setId(15);
-        devashish.setName("Deva Dhalu");
-        devashish.setContact("2456186206");
-        int affectedRows=dbHandler.updateContact(devashish);
-        Log.d("Affected Rows",""+affectedRows);
 
 
-
-        List<CONTACT> allContacts=dbHandler.getAllContacts();
-        Log.d("SIZE_OF_CONTACTS",""+allContacts.size());
-        for(CONTACT contact:allContacts){
-            Log.d("ALL_TYPE_CONTACT",contact.getId()+" "+contact.getName()+" "+contact.getContact());
-        }
-
-        int dr=dbHandler.deleteContact(16);
-        Log.d("Deleted Rows",""+dr);
+        contactArrayList = new ArrayList<>(dbHandler.getAllContacts());
+        MyAdapter myAdapter = new MyAdapter(MainActivity.this,contactArrayList);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
 
 
+
+
+    }
+    private void openAddContactDialog(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.add_contact);
+        dialog.setCancelable(false);
+
+        TextInputEditText contactNameADDCON=dialog.findViewById(R.id.contactNameADDCON);
+        TextInputEditText contactNumberADDCON=dialog.findViewById(R.id.contactNumberADDCON);
+        Button cancelButtonDialog=dialog.findViewById(R.id.cancelButtonDialog);
+        Button okButtonDialog=dialog.findViewById(R.id.okButtonDialog);
+
+
+
+        cancelButtonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        okButtonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CONTACT newContact=new CONTACT();
+
+                newContact.setName(contactNameADDCON.getText().toString().trim());
+                newContact.setContact(contactNumberADDCON.getText().toString().toString());
+
+                dbHandler.addContact(newContact);
+                newContact.setId(dbHandler.getAllContacts().get(dbHandler.getAllContacts().size()-1).getId());
+                contactArrayList.add(newContact);
+                dialog.dismiss();
+                recyclerView.scrollToPosition(contactArrayList.size()-1);
+
+            }
+        });
+
+        dialog.create();
+        dialog.show();
     }
 }
---
+++ app/src/main/java/com/example/onlinedatabase/data/MyAdapter.java
@@ -0,0 +1,174 @@
+package com.example.onlinedatabase.data;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.onlinedatabase.MainActivity;
+import com.example.onlinedatabase.R;
+import com.example.onlinedatabase.model.CONTACT;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<CONTACT> contactList;
+    MyDBHandler myDBHandler;
+
+    public MyAdapter(Context context, ArrayList<CONTACT> contactList){
+        this.context=context;
+        this.contactList=contactList;
+        myDBHandler = new MyDBHandler(context);
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view=layoutInflater.inflate(R.layout.list_item,parent,false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.contactNameTV.setText(contactList.get(position).getName());
+        holder.contactNumberTV.setText(contactList.get(position).getContact());
+        holder.optionsIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view,holder.getAdapterPosition());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+    public void showPopupMenu(View view,int position){
+        PopupMenu popupMenu = new PopupMenu(context,view);
+        popupMenu.inflate(R.menu.edit_delete_menus);
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.edit:{
+                        showDialogBox(position);
+                        popupMenu.dismiss();
+                        break;
+                    }
+                    case R.id.delete: {
+                        deleteItem(position);
+                        popupMenu.dismiss();
+
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
+
+        //Below try catch block is used to force menu icon to appear with the text.
+        try {
+            Field[] fields = popupMenu.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                if ("mPopup".equals(field.getName())) {
+                    field.setAccessible(true);
+                    Object menuPopupHelper = field.get(popupMenu);
+                    Class<?> classPopupHelper = Class.forName(menuPopupHelper
+                            .getClass().getName());
+                    Method setForceIcons = classPopupHelper.getMethod(
+                            "setForceShowIcon", boolean.class);
+                    setForceIcons.invoke(menuPopupHelper, true);
+                    break;
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showDialogBox(int position){
+        Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.add_contact);
+        dialog.setCancelable(false);
+
+        TextInputEditText contactNameADDCON=dialog.findViewById(R.id.contactNameADDCON);
+        TextInputEditText contactNumberADDCON=dialog.findViewById(R.id.contactNumberADDCON);
+        Button cancelButtonDialog=dialog.findViewById(R.id.cancelButtonDialog);
+        Button okButtonDialog=dialog.findViewById(R.id.okButtonDialog);
+
+        contactNameADDCON.setText(contactList.get(position).getName());
+        contactNumberADDCON.setText(contactList.get(position).getContact());
+
+        cancelButtonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        okButtonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CONTACT updatedContact=new CONTACT();
+                updatedContact.setId(contactList.get(position).getId());
+                updatedContact.setName(contactNameADDCON.getText().toString().trim());
+                updatedContact.setContact(contactNumberADDCON.getText().toString().toString());
+
+                myDBHandler.updateContact(updatedContact);
+                MainActivity.contactArrayList = new ArrayList<>(myDBHandler.getAllContacts());
+                MyAdapter myAdapter=new MyAdapter(context,MainActivity.contactArrayList);
+                MainActivity.recyclerView.setAdapter(myAdapter);
+                MainActivity.recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                MainActivity.recyclerView.scrollToPosition(position);
+                dialog.dismiss();
+            }
+        });
+
+        dialog.create();
+        dialog.show();
+
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void deleteItem(int position){
+        myDBHandler.deleteContact(contactList.get(position).getId());
+        MainActivity.contactArrayList.remove(position);
+        notifyDataSetChanged();
+        MainActivity.recyclerView.scrollToPosition(position);
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView contactNameTV;
+        TextView contactNumberTV;
+        ImageView optionsIV;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            contactNameTV=itemView.findViewById(R.id.contactNameTV);
+            contactNumberTV=itemView.findViewById(R.id.contactNumberTV);
+            optionsIV=itemView.findViewById(R.id.optionsIV);
+        }
+    }
+}
